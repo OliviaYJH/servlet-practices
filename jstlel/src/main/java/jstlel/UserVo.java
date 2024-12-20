e7a4e4851347f0ce3066e972f35f1478654fa90d
YuJeongHyun <0703olivia@naver.com>
2024-12-20 15:29:26 +0900

    Servlet02

---
+++ jstlel/src/main/java/jstlel/UserVo.java
@@ -0,0 +1,22 @@
+package jstlel;
+
+public class UserVo {
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long l) {
+		this.id = l;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
